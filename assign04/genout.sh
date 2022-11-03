fa8be07316b61f2f642283ad874652f263f0d047
David Hovemeyer <david.hovemeyer@gmail.com>
2022-11-03 16:09:10 -0400

    generate expected exit codes, start tests for I/O

--- assign04/genout.sh
+++ assign04/genout.sh
@@ -19,5 +19,28 @@ fi
 input_filename="$1"
 stem=$(basename $input_filename .c)
 
+./build.rb ${stem}
+
 $nearly_cc -h $input_filename > example_highlevel_code/${stem}.txt
-$nearly_cc $input_filename > example_lowlevel_code/${stem}.S
+cp out/${stem}.S example_lowlevel_code
+
+# generate expected output and exit code
+mkdir -p expected_output
+
+exit_code='unknown'
+if [ -r data/${stem}.in ]; then
+  ./out/${stem} < data/${stem}.in > expected_output/${stem}.out
+  exit_code=$?
+else
+  ./out/${stem} > expected_output/${stem}.out
+  exit_code=$?
+fi
+
+# delete expected output if no output was produced
+if [ ! -s "expected_output/${stem}.out" ]; then
+  echo "expected_output/${stem}.out is empty, deleting it"
+  rm -f expected_output/${stem}.out
+fi
+
+mkdir -p expected_exit_code
+echo "${exit_code}" > expected_exit_code/${stem}.out
