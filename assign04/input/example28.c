afbc1f5ee12e4c81ad8d09bc9e62b9fe7e036a51
David Hovemeyer <david.hovemeyer@gmail.com>
2022-11-09 08:37:44 -0500

    initial matrix multiplication program
    need to fix local storage allocation bug before it
    will work

--- assign04/input/example28.c
+++ assign04/input/example28.c
@@ -1,42 +1,54 @@
-// this program multiplies 10x10 square matrices,
+// This program multiplies 10x10 square matrices,
 // represented as 100 element 1-D arrays
 
-void read_m10x10(long arr[]) {
+long read_i64(void);
+void print_i64(long n);
+void print_nl(void);
+
+void read_m10x10(long arr[0]) {
   int i;
-  for (int i = 0; i < 100; i = i + 1) {
+  for (i = 0; i < 100; i = i + 1) {
     arr[i] = read_i64();
   }
 }
 
 int main(void) {
   long a[100], b[100], c[100];
+  int M;
+  int i, j,  k;
+  long r, val;
+
+  M = 10;
 
   // read input matrices
   read_m10x10(a);
   read_m10x10(b);
 
-  // do matrix multiplication
+  // initialize result matrix to all 0s
+  for (i = 0; i < 100; i = i + 1) {
+    c[i] = 0L;
+  }
 
-/*
-  k := 0;
-  WHILE k < M DO
-    i := 0;
-    WHILE i < M DO
-      r := a[i * M + k];
-      j := 0;
-      WHILE j < M DO
-        val := c[i * M + j];
-        val := val + (r * b[k * M + j]);
-        c[i * M + j] := val;
-        j := j + 1;
-      END;
-      i := i + 1;
-    END;
-    k := k + 1;
-  END;
-*/
-  int i, j,  k;
+  // do the matrix multiplication: note that the
+  // loops are structured to avoid iterating over a column
+  // of elements and incurring excessive cache misses
+  // as a result
+  for (k = 0; k < M; k = k + 1) {
+    for (i = 0; i < M; i = i + 1) {
+      r = a[i*M + k];
+      for (j = 0; j < M; j = j + 1) {
+        val = c[i*M + j];
+        val = val + (r * b[k*M + j]);
+        c[i*M + j] = val;
+      }
+    }
+  }
 
-  
+  // print the elements of the product matrix
+  for (i = 0; i < 100; i = i + 1) {
+    print_i64(c[i]);
+    print_nl();
+  }
 
+  return 0;
 }
